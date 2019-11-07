99f015b54a8884fa2b8078fab5c8d4a7d5089cf3
Dustin <clerin+gitkraken@gmail.com>
2019-11-06 18:22:17 -0800

    finished rewriting eating_cookies to use cached values.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,22 +8,30 @@ import sys
 def eating_cookies(n, cache=None):
     if n < 0: return 0
     if n == 0: return 1
+    global recursion
+    if not isinstance(cache, dict):
+        cache = {}
     
     ways_to_eat = [3,2,1]; diff_ways = 0; i = 0
 
-    def find_ways_to_eat(ttl_cookies, j):
-        if ttl_cookies - ways_to_eat[j] == 0:
-           nonlocal diff_ways
-           diff_ways += 1
-        elif ttl_cookies - ways_to_eat[j] > 0:
-            ttl_cookies -= ways_to_eat[j]
-            for b, way in enumerate(ways_to_eat):
-                find_ways_to_eat(ttl_cookies, b)
-    
-    for i, way in enumerate(ways_to_eat):
-        find_ways_to_eat(n, i)
+    if n in cache:
+        # print(f"from cache: {cache[n]}")
+        return cache[n]
+    else:
+        # print(f"calculate: {n}")
+        def find_ways_to_eat(ttl_cookies, j):
+            if ttl_cookies - ways_to_eat[j] == 0:
+                nonlocal diff_ways
+                diff_ways += 1
+            elif ttl_cookies - ways_to_eat[j] > 0:
+                ttl_cookies -= ways_to_eat[j]
+                diff_ways += eating_cookies(ttl_cookies, cache)
+        
+        for i, way in enumerate(ways_to_eat):
+            find_ways_to_eat(n, i)
 
-    return diff_ways
+        cache[n] = diff_ways
+        return diff_ways
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
